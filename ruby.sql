2a02e849d5a149624e1da1382c369eb5bd83b92e
Dmytro <dimalopp@gmail.com>
2016-09-15 22:07:14 +0300

    add new sql

--- ruby.sql
+++ ruby.sql
@@ -1,40 +1,50 @@
-/*
-Navicat MySQL Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.10
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Сен 15 2016 г., 22:05
+-- Версия сервера: 5.5.45
+-- Версия PHP: 5.6.12
 
-Source Server         : denver
-Source Server Version : 50545
-Source Host           : localhost:3306
-Source Database       : ruby
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-Target Server Type    : MYSQL
-Target Server Version : 50545
-File Encoding         : 65001
 
-Date: 2016-09-01 01:33:41
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- База данных: `ruby`
+--
 
--- ----------------------------
--- Table structure for `ru_projects`
--- ----------------------------
-DROP TABLE IF EXISTS `ru_projects`;
-CREATE TABLE `ru_projects` (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `ru_projects`
+--
+
+CREATE TABLE IF NOT EXISTS `ru_projects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `status` int(1) DEFAULT NULL,
   `create` int(11) DEFAULT NULL,
   `update` int(11) DEFAULT NULL,
   `order` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `order` (`order`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- ----------------------------
--- Table structure for `ru_tasks`
--- ----------------------------
-DROP TABLE IF EXISTS `ru_tasks`;
-CREATE TABLE `ru_tasks` (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `ru_tasks`
+--
+
+CREATE TABLE IF NOT EXISTS `ru_tasks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `status` int(1) DEFAULT NULL,
@@ -42,9 +52,24 @@ CREATE TABLE `ru_tasks` (
   `create` int(11) DEFAULT NULL,
   `update` int(11) DEFAULT NULL,
   `project_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `ru_users`
+--
+
+CREATE TABLE IF NOT EXISTS `ru_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) DEFAULT NULL,
+  `hash` varchar(255) DEFAULT NULL,
+  `create` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `order` (`order`),
-  KEY `order` (`project_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
